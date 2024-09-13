4191dd8b38f8c20b50525ae12f1926babddf44a0
Kraft <147241506+Kraft42@users.noreply.github.com>
2024-09-13 11:19:53 +0800

    working on it

---
+++ proj1b/src/ArryDeque61B.java
@@ -0,0 +1,52 @@
+import java.util.List;
+
+public class ArryDeque61B<T> implements Deque61B<T> {
+    public ArryDeque61B(){
+        
+    }
+
+    @Override
+    public void addFirst(T x) {
+
+    }
+
+    @Override
+    public void addLast(T x) {
+
+    }
+
+    @Override
+    public List<T> toList() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public T removeFirst() {
+        return null;
+    }
+
+    @Override
+    public T removeLast() {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        return null;
+    }
+}
