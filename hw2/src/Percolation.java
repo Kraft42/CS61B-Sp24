fabc1188178af9f68711087c4bdab4b9836b3da4
Kraft <147241506+Kraft42@users.noreply.github.com>
2024-09-24 22:05:01 +0800

    Complete hw2

--- hw2/src/Percolation.java
+++ hw2/src/Percolation.java
@@ -3,8 +3,9 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     // TODO: Add any necessary instance variables.
-    private int[][] grid;
+    private boolean[][] grid;
     private WeightedQuickUnionUF uf;
+    private int virtual_top,vritual_bottom;
 
     public Percolation(int N) {
         // TODO: Fill in this constructor.
@@ -12,14 +13,20 @@ public class Percolation {
         if(N <= 0)
             throw new IllegalArgumentException("N must be bigger than 0.");
 
-        grid = new int[N][N];
+        grid = new boolean[N][N];
         for(int r = 0;r < N;r++){
             for(int c = 0;c < N;c++){
-                grid[r][c] = -1;
+                grid[r][c] = false;
             }
         }
 
-        uf = new WeightedQuickUnionUF(N*N);
+        uf = new WeightedQuickUnionUF(N*N+2);
+        virtual_top = N*N;
+        vritual_bottom = N*N+1;
+    }
+
+    private int tr_h(int row, int col){
+        return row * grid.length + col;
     }
 
     public void open(int row, int col) {
@@ -27,53 +34,23 @@ public class Percolation {
         if(row<0 || row>=grid.length || col<0 || col>=grid.length)
             throw new IndexOutOfBoundsException();
 
-        if(grid[row][col] == -1) {
-            grid[row][col] = 0;
-            fill_full(row,col);
-            if(grid[row][col] == 1)
-                make_full(row,col);
+        grid[row][col] = true;
+        if(row == 0)
+            uf.union(tr_h(row, col),virtual_top);
+        if(row == grid.length-1)
+            uf.union(tr_h(row,col),vritual_bottom);
+
+        if(grid[Math.max(row - 1, 0)][col]){
+            uf.union(tr_h(row,col), tr_h(Math.max(row-1,0),col));
         }
-
-        if(row == 0){
-            grid[row][col] = 1;
-            make_full(row, col);
+        if(grid[row][Math.max(col - 1, 0)]){
+            uf.union(tr_h(row,col), tr_h(row,Math.max(col-1,0)));
         }
-
-    }
-
-    // When a site is open,check if it can be filled full.
-    public void fill_full(int row,int col){
-        assert grid[row][col] == 0;
-
-        if(grid[Math.max(row-1,0)][col] == 1)
-            grid[row][col] = 1;
-        else if(grid[row][Math.max(col-1,0)] == 1)
-            grid[row][col] = 1;
-        else if(grid[Math.min(row+1,grid.length-1)][col] == 1)
-            grid[row][col] = 1;
-        else if(grid[row][Math.min(col+1,grid.length-1)] == 1)
-            grid[row][col] = 1;
-    }
-
-    // When a site is full,check if it can make near sites full.
-    public void make_full(int row,int col){
-        assert grid[row][col] == 1;
-
-        if(grid[Math.max(row-1,0)][col] == 0){
-            grid[Math.max(row-1,0)][col] = 1;
-            make_full(Math.max(row-1,0),col);
+        if(grid[Math.min(row + 1, grid.length - 1)][col]){
+            uf.union(tr_h(row,col), tr_h(Math.min(row+1,grid.length-1),col));
         }
-        if(grid[row][Math.max(col-1,0)] == 0){
-            grid[row][Math.max(col-1,0)] = 1;
-            make_full(row,Math.max(col-1,0));
-        }
-        if(grid[Math.min(row+1,grid.length-1)][col] == 0) {
-            grid[Math.min(row + 1, grid.length - 1)][col] = 1;
-            make_full(Math.min(row+1,grid.length-1),col);
-        }
-        if(grid[row][Math.min(col+1,grid.length-1)] == 0) {
-            grid[row][Math.min(col + 1, grid.length - 1)] = 1;
-            make_full(row,Math.min(col+1, grid.length-1));
+        if(grid[row][Math.min(col + 1, grid.length - 1)]){
+            uf.union(tr_h(row,col), tr_h(row,Math.min(col+1,grid.length-1)));
         }
     }
 
@@ -82,7 +59,7 @@ public class Percolation {
         if(row<0 || row>=grid.length || col<0 || col>=grid.length)
             throw new IndexOutOfBoundsException();
 
-        return grid[row][col] != -1;
+        return grid[row][col];
     }
 
     public boolean isFull(int row, int col) {
@@ -90,28 +67,17 @@ public class Percolation {
         if(row<0 || row>=grid.length || col<0 || col>=grid.length)
             throw new IndexOutOfBoundsException();
 
-        return grid[row][col] == 1;
+        return uf.connected(tr_h(row,col),virtual_top);
     }
 
     public int numberOfOpenSites() {
         // TODO: Fill in this method.
-        int cnt = 0;
-        for(int r = 0;r < grid.length;r++){
-            for(int c = 0;c < grid.length;c++){
-                if(grid[r][c] != -1)
-                    cnt++;
-            }
-        }
-        return cnt;
+        return grid.length*grid.length - uf.count();
     }
 
     public boolean percolates() {
         // TODO: Fill in this method.
-        for(int i = 0;i < grid.length;i++){
-            if(grid[grid.length-1][i] == 1)
-                return true;
-        }
-        return false;
+        return uf.connected(virtual_top,vritual_bottom);
     }
 
     // TODO: Add any useful helper methods (we highly recommend this!).
