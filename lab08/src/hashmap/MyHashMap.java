5fcbe8508022b98a6b387def358ab3b04bad353f
Kraft <147241506+Kraft42@users.noreply.github.com>
2024-10-09 09:39:28 +0800

    Did not complete yet

--- lab08/src/hashmap/MyHashMap.java
+++ lab08/src/hashmap/MyHashMap.java
@@ -1,6 +1,9 @@
 package hashmap;
 
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Set;
 
 /**
  *  A hash table-backed Map implementation.
@@ -9,7 +12,6 @@ import java.util.Collection;
  *  @author YOUR NAME HERE
  */
 public class MyHashMap<K, V> implements Map61B<K, V> {
-
     /**
      * Protected helper class to store key/value pairs
      * The protected qualifier allows subclass access
@@ -26,12 +28,28 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
 
     /* Instance Variables */
     private Collection<Node>[] buckets;
+    private int Capacity;
+    private double loadFactor;
     // You should probably define some more!
 
     /** Constructors */
-    public MyHashMap() { }
+    public MyHashMap() {
+        Capacity = 16;
+        loadFactor = 0.75;
+        buckets = new Collection[Capacity];
+        for(Collection<Node> bucket:buckets){
+            bucket = createBucket();
+        }
+    }
 
-    public MyHashMap(int initialCapacity) { }
+    public MyHashMap(int initialCapacity) {
+        Capacity = initialCapacity;
+        loadFactor = 0.75;
+        buckets = new Collection[Capacity];
+        for(Collection<Node> bucket:buckets){
+            bucket = createBucket();
+        }
+    }
 
     /**
      * MyHashMap constructor that creates a backing array of initialCapacity.
@@ -40,7 +58,14 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * @param initialCapacity initial size of backing array
      * @param loadFactor maximum load factor
      */
-    public MyHashMap(int initialCapacity, double loadFactor) { }
+    public MyHashMap(int initialCapacity, double loadFactor) {
+        Capacity = initialCapacity;
+        this.loadFactor = loadFactor;
+        buckets = new Collection[Capacity];
+        for(Collection<Node> bucket:buckets){
+            bucket = createBucket();
+        }
+    }
 
     /**
      * Returns a data structure to be a hash table bucket
@@ -64,10 +89,74 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      */
     protected Collection<Node> createBucket() {
         // TODO: Fill in this method.
-        return null;
+        return new LinkedList<>();
     }
 
     // TODO: Implement the methods of the Map61B Interface below
     // Your code won't compile until you do so!
+    private void resize(){}
+
+    @Override
+    public void put(K key, V value) {
+        if((double) size() /Capacity >= loadFactor){
+            resize();
+        }
+
+        Node newNode = new Node(key,value);
+        int bucketNum = Math.floorMod(key.hashCode(),Capacity);
+        if(buckets[bucketNum] == null){
+            buckets[bucketNum] = createBucket();
+        }
+        buckets[bucketNum].add(newNode);
+    }
+
+    @Override
+    public V get(K key) {
+        int bucketNum = Math.floorMod(key.hashCode(),Capacity);
+        for(Node n:buckets[bucketNum]){
+            if(n.key.equals(key))
+                return n.value;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        int bucketNum = Math.floorMod(key.hashCode(),Capacity);
+        for(Node n:buckets[bucketNum]){
+            if(n.key.equals(key))
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int size() {
+        int s = 0;
+        for (Collection<Node> bucket : buckets) {
+            s += bucket.size();
+        }
+        return s;
+    }
+
+    @Override
+    public void clear() {
+        buckets = new Collection[16];
+    }
+
+    @Override
+    public Set<K> keySet() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        throw new UnsupportedOperationException();
+    }
 
 }
